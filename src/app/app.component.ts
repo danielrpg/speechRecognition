cdfbaad0a1195c7edf94e80194aa22eb1e5f28a2
danielrpg <daniel.fernandezgp@gmail.com>
2017-08-20 21:31:18 -0400

    Changes in voice recognition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+declare const navigator:any;
+declare const MediaRecorder:any;
 
 @Component({
+  moduleId : module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
